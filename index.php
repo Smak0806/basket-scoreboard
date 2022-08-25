4d2b7d22bdaa9691c2ae7544b67f3003267ffd27
torodevelolpment <isaacrafaeltoroguzman@gmail.com>
2022-08-25 20:10:28 +0200

    se añaden links de bootstrap (css & script)

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ print_r("hola mundo");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <title>Bask</title>
 </head>
 <body>
@@ -25,5 +26,6 @@ print_r("hola mundo");
         
 
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
 </html>
