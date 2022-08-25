43a17eb3413067a66d19bfd0fd668df70c74d715
santi <santiago.cabrera0806@gmail.com>
2022-08-25 19:55:46 +0200

    añado archivos

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+    
+
+print_r("hola mundo");
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bask</title>
+</head>
+<body>
+    <div class="container">
+        <div id="marcador">
+            <section id="tituloMarcador"></section>
+        </div>
+        
+
+    </div>
+</body>
+</html>
